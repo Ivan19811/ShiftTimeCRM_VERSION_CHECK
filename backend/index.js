4d7c1c4c03a8e3372be418f7b93f38e1e0803778
Ivan19811 <147108377+Ivan19811@users.noreply.github.com>
2025-06-03 00:22:17 +0300

    🐛 Fixed path to version.json for Render

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 
 app.get('/version', (req, res) => {
-  const filePath = path.join(__dirname, 'backend', 'version.json');
+  const filePath = path.join(__dirname, 'version.json');
 
   const data = fs.readFileSync(filePath, 'utf8');
   res.setHeader('Content-Type', 'application/json');
